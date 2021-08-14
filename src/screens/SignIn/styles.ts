eb4815054cd579bcc7a34ccbd7c56a74806535da
bruno <bruno.duarte@aluno.uece.br>
2021-08-14 16:34:29 -0300

    End of class 1

---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,30 @@
+import {StyleSheet} from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: theme.colors.background,
+    },
+    image: {
+        width: '100%',
+        height: 360,
+    },
+    content: {
+        marginTop: -80,
+        paddingHorizontal: 50,
+    },
+    title: {
+        color: theme.colors.heading,
+        textAlign: 'center',
+        fontSize: 40,
+        marginBottom: 8,
+    },
+    subtitle: {
+        color: theme.colors.heading,
+        textAlign: 'center',
+        marginBottom: 32,
+    },
+});
